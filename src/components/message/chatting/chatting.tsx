e0814373488ec258d47f74b699a8852e5cde4885
sanadevel <a36133353@gmail.com>
2024-03-21 21:04:16 +0900

    ✨ : Message Send Function Development

--- src/components/message/chatting/chatting.tsx
+++ src/components/message/chatting/chatting.tsx
@@ -1,24 +1,51 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faImage,
-  faInfoCircle,
-  faPaperPlane,
-} from "@fortawesome/free-solid-svg-icons";
+import { faImage, faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
+import { msgTypes } from "../../../types/msgType";
 import * as S from "./style";
 
 const ChattingTab = () => {
+  const [msgContent, setMsgContent] = useState("");
+  const [msgContents, setMsgContents] = useState<msgTypes[]>([]);
+
+  const onMsgSend = (event: any) => {
+    if (
+      event.nativeEvent.isComposing === false && // 2번 입력되는 거 방지
+      event.code === "Enter" &&
+      msgContent
+    ) {
+      console.log(msgContent);
+      setMsgContents([
+        { chatterType: "self", message: msgContent, msgId: 1 },
+        ...msgContents,
+      ]);
+      setMsgContent("");
+    }
+  };
+
   return (
     <S.ChattingTab>
       <S.ChatWindow>
-        <S.ChatInput type="text" placeholder="메세지 입력..." />
+        <S.ChatInput
+          onChange={(e) => setMsgContent(e.target.value)}
+          value={msgContent}
+          type="text"
+          placeholder="메세지 입력..."
+          onKeyUp={(e) => onMsgSend(e)} // onKeyPress 지양하기
+        />
         <FontAwesomeIcon icon={faImage} size="2xl" />
         <FontAwesomeIcon icon={faPaperPlane} size="2xl" />
       </S.ChatWindow>
       <S.ChatContents>
-        <S.ChatMsg chatterType="self">안녕하세요</S.ChatMsg>
-        <S.ChatMsg chatterType="self">안녕하세요</S.ChatMsg>
-        <S.ChatMsg chatterType="opponent">안녕하세요</S.ChatMsg>
+        {msgContents?.map(({ chatterType, message, msgId }) => {
+          return (
+            <S.ChatMsg chatterType={chatterType} key={msgId}>
+              {message}
+            </S.ChatMsg>
+          );
+        })}
+        <S.ChatMsg chatterType="opponent">ㄴㅁㅇㄹ</S.ChatMsg>
       </S.ChatContents>
       <S.ChatProfile>
         <S.TargetInfo>

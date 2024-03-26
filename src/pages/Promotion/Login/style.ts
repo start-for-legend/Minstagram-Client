92b3bf3ab933b435159b4fe7ec0cdd7376a776ec
sanadevel <a36133353@gmail.com>
2024-03-26 15:16:07 +0900

    ♻️ : Refactoring LoginForm

--- src/pages/Promotion/Login/style.ts
+++ src/pages/Promotion/Login/style.ts
@@ -7,20 +7,6 @@ export const LoginForm = styled.div`
   justify-content: center;
 `;
 
-export const LoginBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  border: 1px solid #c5c5c5;
-  height: auto;
-  width: 22em;
-  svg {
-    margin-top: 1em;
-    margin-bottom: 1em;
-  }
-`;
-
 export const IdInput = styled.input`
   width: 20rem;
   height: 2em;
@@ -43,7 +29,7 @@ export const LoginBtn = styled.button`
 export const OtherTool = styled.div`
   float: left;
   margin-top: 1em;
-  margin-bottom: 2em;
+  margin-bottom: 0.5em;
   span {
     margin-left: 1.25rem;
     margin-right: 1.25rem;
@@ -62,23 +48,4 @@ export const AnyBox = styled.div`
   line-height: 2.5rem;
   margin-bottom: 1em;
   margin-top: 0.5em;
-`;
-
-export const ForgetPw = styled.div`
-  margin-bottom: 1em;
-`;
-
-export const RegisterBox = styled.div`
-  margin-top: 1em;
-  width: 22em;
-  border: 1px solid #c5c5c5;
-  padding-top: 1em;
-  padding-bottom: 1em;
-  text-align: center;
-`;
-
-export const Register = styled.span`
-  cursor: pointer;
-  color: blue;
-  font-weight: 500;
 `;
--- src/pages/Promotion/index.tsx
+++ src/pages/Promotion/index.tsx
@@ -1,15 +1,26 @@
+import { useState } from "react";
+
 import Footer from "../../components/footer";
 import LoginForm from "./Login";
-import { SIUU } from "../../assets/files";
+import SignUpForm from "./SignUp";
+import { LightLogo, SIUU } from "../../assets/files";
 import * as S from "./style";
 
 const Promotion = () => {
+  const [loginState, setLoginState] = useState(false);
   return (
     <S.PromotionContainer>
       <S.Minstagram>Minstagram</S.Minstagram>
       <S.PromotionBox>
         <SIUU height="37em" />
-        <LoginForm />
+        <S.AccountBox>
+          <LightLogo height="10em" />
+          {loginState ? <SignUpForm /> : <LoginForm />}
+          계정이 없나요?
+          <S.Register onClick={() => setLoginState(!loginState)}>
+            가입하기
+          </S.Register>
+        </S.AccountBox>
       </S.PromotionBox>
       <Footer />
     </S.PromotionContainer>

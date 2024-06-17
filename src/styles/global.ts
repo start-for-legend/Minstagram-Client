d0c7d74d666dadfbca4980efb991271e93fc998f
Sanadevel <a36133353@gmail.com>
2024-06-17 15:10:29 +0900

    💄 : Change Global Style

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -14,6 +14,7 @@ const GlobalStyle = createGlobalStyle`
       src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_suit@1.0/SUIT-Regular.woff2') format('woff2');
       font-weight: normal;
       font-style: normal;
+      font-display: swap;
       }
       body {
     padding: 0;
